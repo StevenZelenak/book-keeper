f27885aa6e8034f1103a297bfebf697ddc0a9f5a
Steven Zelenak <stevenmzelenak@gmail.com>
2020-06-24 20:13:13 -0500

    Can add books!

--- src/components/pages/NewBook/NewBook.js
+++ src/components/pages/NewBook/NewBook.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
 import './NewBook.scss';
 import authData from '../../../helpers/data/authData';
 import bookData from '../../../helpers/data/bookData';
@@ -42,17 +43,17 @@ class NewStuff extends React.Component {
 
   typeChange = (e) => {
     e.preventDefault();
-    this.setState({ bookType: e.target.value });
+    this.setState({ bookType: e.target.id });
   }
 
   genreChange = (e) => {
     e.preventDefault();
-    this.setState({ bookGenre: e.target.value });
+    this.setState({ bookGenre: e.target.id });
   }
 
   statusChange = (e) => {
     e.preventDefault();
-    this.setState({ bookStatus: e.target.value });
+    this.setState({ bookStatus: e.target.id });
   }
 
   favoriteChange = (e) => {
@@ -97,9 +98,6 @@ class NewStuff extends React.Component {
       bookName,
       bookImage,
       bookAuthor,
-      bookType,
-      bookGenre,
-      bookStatus,
       bookFavorite,
       bookNarrator,
     } = this.state;
@@ -158,18 +156,57 @@ class NewStuff extends React.Component {
               />
             <label className="form-check-label" htmlFor="scat-wasFulfilling">Favorite</label>
           </div>
-          <div className="form-group" >
-            <div class="dropdown">
-              <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <div className="d-flex row justify-content-center">
+          <div className="form-group mx-3" >
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
                 Genre
-              </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                  <button class="dropdown-item" value='genre1' type="button" onClick={this.genreChange}>Action</button>
-                  <button class="dropdown-item" value='genre2' type="button" onClick={this.genreChange}>Adventure</button>
-                </div>
-              </div>
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item id="genre1" onClick={this.genreChange}>Action</Dropdown.Item>
+                <Dropdown.Item id="genre2" onClick={this.genreChange}>Adventure</Dropdown.Item>
+                <Dropdown.Item id="genre3" onClick={this.genreChange}>Comedy</Dropdown.Item>
+                <Dropdown.Item id="genre4" onClick={this.genreChange}>Crime</Dropdown.Item>
+                <Dropdown.Item id="genre5" onClick={this.genreChange}>Drama</Dropdown.Item>
+                <Dropdown.Item id="genre6" onClick={this.genreChange}>Fantasy</Dropdown.Item>
+                <Dropdown.Item id="genre7" onClick={this.genreChange}>Horror</Dropdown.Item>
+                <Dropdown.Item id="genre8" onClick={this.genreChange}>Romance</Dropdown.Item>
+                <Dropdown.Item id="genre9" onClick={this.genreChange}>Political</Dropdown.Item>
+                <Dropdown.Item id="genre10" onClick={this.genreChange}>Historical</Dropdown.Item>
+                <Dropdown.Item id="genre11" onClick={this.genreChange}>Mystery</Dropdown.Item>
+                <Dropdown.Item id="genre12" onClick={this.genreChange}>Science Fiction</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
           </div>
+          <div className="form-group mx-3" >
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Type
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item id="type1" onClick={this.typeChange}>Audio</Dropdown.Item>
+                <Dropdown.Item id="type2" onClick={this.typeChange}>Digital</Dropdown.Item>
+                <Dropdown.Item id="type3" onClick={this.typeChange}>Physical</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+          <div className="form-group mx-3" >
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Status
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item id="status1" onClick={this.statusChange}>Reading</Dropdown.Item>
+                <Dropdown.Item id="status2" onClick={this.statusChange}>Finished</Dropdown.Item>
+                <Dropdown.Item id="status3" onClick={this.statusChange}>Wishlist</Dropdown.Item>
+                <Dropdown.Item id="status4" onClick={this.statusChange}>n/a</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+          </div>
+          <div className="d-flex row justify-content-center mt-3">
           <button className="btn btn-primary" onClick={this.saveItem}>Save Item</button>
+          </div>
         </form>
       </div>
     );
